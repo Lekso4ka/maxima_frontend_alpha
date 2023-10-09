02bf051cb64d60429386c2cf1bf2b383a64ce90a
gemansipator <gemansipator@gmail.com>
2023-10-09 14:23:19 +0300

    Ошибка при попытке прописать map в шаблоне (компоненте) Table

--- src/components/table/Table.jsx
+++ src/components/table/Table.jsx
@@ -1,16 +1,12 @@
 import React, {useState} from 'react';
 import './Table.scss';
+import {CheckOutlined} from "@ant-design/icons";
 
 
 import {SettingOutlined} from "@ant-design/icons";
 
 const Table = (
-    {
-        title,
-        createdAt,
-        text
-
-    }
+    news
 ) => {
 
     return (
@@ -19,17 +15,37 @@ const Table = (
                 <th> Название</th>
                 <th>Дата</th>
                 <th> Текст</th>
+                <th> Видимость <br/>преподаватели</th>
+                <th> Видимость <br/>студенты</th>
                 <th>Управление</th>
             </tr>
-            <tr>
-                <td>{title}</td>
-                <td>Дата: ("{createdAt}")</td>
-                <td>{text}</td>
+            {news.map(el =>
+                <tr>
+                    <td>{el.title}</td>
+                    <td>Дата: ("{el.createdAt}")</td>
+                    <td>{el.text}</td>
+                    <td>{el.forTeachers && <CheckOutlined />}
+                    </td>
+                    <td>{el.forStudents && <CheckOutlined />}
+                    </td>
 
-                <td className="image-button" >
-                    <SettingOutlined style={{ width: 100 }}/>
-                </td>
-            </tr>
+                    <td className="image-button">
+                        <SettingOutlined style={{width: 100}}/>
+                    </td>
+                </tr>)
+            }
+            {/*<tr>*/}
+            {/*    <td>{news.title}</td>*/}
+            {/*    <td>Дата: ("{news.createdAt}")</td>*/}
+            {/*    <td>{news.text}</td>*/}
+            {/*    <td>{news.forTeachers && <CheckOutlined/>}</td>*/}
+            {/*    <td>{news.forStudents && <CheckOutlined/>}</td>*/}
+            {/*    <td className="image-button" >*/}
+            {/*        <SettingOutlined style={{ width: 100 }}/>*/}
+            {/*    </td>*/}
+            {/*</tr>*/}
+
+
         </table>
     );
 };
