dd5751b303b6d941fde126a4ed57fe511ecdfcbe
NastyaLomakina <gemansipator1@gmail.com>
2023-10-11 20:27:47 +0300

    доделала таблицу пользователей

--- src/components/userstable/UsersTable.jsx
+++ src/components/userstable/UsersTable.jsx
@@ -8,17 +8,7 @@ const UsersTable = (
 ) => {
   
   return(
-  <table className='userstable'>
-      <tbody>
-        <thead>
-          <tr>
-            <th>Ф.И.О.</th>
-            <th>Логин</th>
-            <th>Роль</th>
-            <th>Возраст</th>
-            <th>Электронный адрес</th>
-            <th>Управление</th>
-          </tr>
+
           <tr>
             <td>{users.lastName} {users.firstName} {users.patronymic}</td>
             <td>{users.login}</td>
@@ -27,9 +17,6 @@ const UsersTable = (
             <td>{users.email}</td>
             <td className='image-button'><SettingOutlined style ={{width: 150}}/></td>
           </tr>
-        </thead>
-      </tbody>
-    </table>
     )
 }
 
--- src/pages/UsersPage/UsersPage.js
+++ src/pages/UsersPage/UsersPage.js
@@ -4,11 +4,29 @@ import users from "../../assets/data/users.json";
 function UsersPage () {
     return (
         <>
-        
-        {users.map(item => <UsersTable role ={item.role} firstName ={item.firstName} lastName={item.lastName}
-        patronymic={item.patronymic} email={item.email} login={item.login} gender={item.gender}
-        birthDate={item.birthDate}/> )}
-        </>
+            <table className='userstable'>
+                <tbody>
+                    <thead>
+                        <tr>
+                            <th>Ф.И.О.</th>
+                            <th>Логин</th>
+                            <th>Роль</th>
+                            <th>Возраст</th>
+                            <th>Электронный адрес</th>
+                            <th>Управление</th>
+                        </tr>
+
+
+                    </thead>
+                    <tr>
+                        {users.map(item => <UsersTable role ={item.role} firstName ={item.firstName} lastName={item.lastName}
+                                                       patronymic={item.patronymic} email={item.email} login={item.login} gender={item.gender}
+                                                       birthDate={item.birthDate}/> )}
+                    </tr>
+                </tbody>
+            </table>
+                </>
+
     )
 }
 
