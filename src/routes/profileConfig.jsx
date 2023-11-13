d8beb5fd11640a5b036a47b1ebba6e4b85c0b00e
gemansipator <gemansipator@gmail.com>
2023-11-14 00:22:57 +0300

    Переместил роутеры обратно как было изначально, добавил

--- src/routes/profileConfig.jsx
+++ src/routes/profileConfig.jsx
@@ -1,33 +1,50 @@
 import Profile from '../pages/Profile'
 import NewsPage from "../pages/NewsPage/NewsPage";
 import Users from '../pages/UsersPage/UsersPage';
+import MaterialsPage from "../pages/MaterialsPage/MaterialsPage";
+import CoursesPage from "../pages/CoursesPage/CoursesPage";
 
 
 export const profileConfig = {
     "PARTNER": {
         menu: [
 
-            // {
-            //     name: 'Главная',
-            //     path: '/',
-            //     component: <Profile/>,
-            //     isMenuItem: true,
-            //     exact: true,
-            // },
-            // {
-            //     name: 'Новости',
-            //     path: '/news',
-            //     component: <NewsPage />,
-            //     isMenuItem: true,
-            //     exact: true,
-            // },
-            // {
-            //     name: 'Пользователи',
-            //     path: '/users',
-            //     component: <Users />,
-            //     isMenuItem: true,
-            //     exact: true,
-            // },
+            {
+                name: 'Главная',
+                path: '/',
+                component: <Profile/>,
+                isMenuItem: true,
+                exact: true,
+            },
+            {
+                name: 'Курсы',
+                path: '/courses',
+                component: <CoursesPage />,
+                isMenuItem: true,
+                exact: true,
+            },
+            {
+                name: 'Новости',
+                path: '/news',
+                component: <NewsPage />,
+                isMenuItem: true,
+                exact: true,
+            },
+            {
+                name: 'Пользователи',
+                path: '/users',
+                component: <Users />,
+                isMenuItem: true,
+                exact: true,
+            },
+            {
+                name: 'Материалы',
+                path: '/materials',
+                component: <MaterialsPage />,
+                isMenuItem: true,
+                exact: true,
+            },
+
         ],
     },
     "STUDENT": {
